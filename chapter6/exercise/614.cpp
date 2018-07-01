9fc01756a5f3e5104e1b6c2c189fe0d5e0c8efe0
realme72 <gauravbharti586@gmail.com>
2018-07-01 18:00:58 +0530

    little change

---
+++ chapter6/exercise/614.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int sum(int a,int b);
+int main()
+{
+	sum(1,2);
+	return 0;
+}
+int sum(int a,int b)
+{
+	printf("sum is %d\n",a+b);
+	return a+b;
+}
