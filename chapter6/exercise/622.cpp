877c4c25364766f7f93622b93a5a10f802ca11cb
realme72 <gauravbharti586@gmail.com>
2018-07-02 17:33:50 +0530

    global variable

---
+++ chapter6/exercise/622.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a=5;
+void func();
+int main()
+{
+	func();
+	printf("%d\n",a);
+	return 0;
+}
+
+void func()
+{
+	int a=2;
+	printf("%d\n",a);
+}
