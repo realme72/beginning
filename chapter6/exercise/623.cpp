9e71ce0112f59d61e6175ae3c0e5a0696fe5d561
realme72 <gauravbharti586@gmail.com>
2018-07-02 17:46:19 +0530

    good question

---
+++ chapter6/exercise/623.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int func(int x,int y);
+
+int main()
+{
+	int a,b;
+	printf("enter values for a and b\n");
+	scanf("%d%d",&a,&b);
+	a=func(a+b,a-b);
+	printf("%d\n",a);
+	return 0;
+}
+
+int  func(int x,int y)
+{
+	return x+y,x-y;
+}
