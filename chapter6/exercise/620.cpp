8b585e546363e0492de450c84cbb2a2bc3319a62
realme72 <gauravbharti586@gmail.com>
2018-07-02 09:36:02 +0530

    important thing to observe

---
+++ chapter6/exercise/620.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int func(int a,int b,int c);
+
+int main()
+{
+	int x;
+	x= func(2,3,4);
+	printf("%d\n",x);
+	return 0;
+}
+
+int func(int a,int b,int c)
+{
+	return a,b,c;
+}
