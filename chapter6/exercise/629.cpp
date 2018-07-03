452967fcc889b871ce4bff9b883a1a733a293ca6
realme72 <gauravbharti586@gmail.com>
2018-07-03 12:50:03 +0530

    simple use of function

---
+++ chapter6/exercise/629.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int func(int a,int b);
+int f1(int x,int y);
+int f2(int x,int y);
+
+int main()
+{
+	int a,b;
+	printf("enter a and b\n");
+	scanf("%d%d",&a,&b);
+	printf("%d\n",func(a,b));
+	return 0;
+}
+
+int func(int a,int b)
+{
+	int x;
+	if(a==b)
+	{
+			return f1(a,b);
+
+	}	
+	return f2(a,b);
+}
+int f1(int x,int y)
+{
+	return x+y;
+}
+int f2(int x,int y)
+{
+	return x-y;
+}
