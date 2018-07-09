dfbb7289a7dab5fc5b061285ed25ea498d74b3fd
realme72 <gauravbharti586@gmail.com>
2018-07-09 17:53:33 +0530

    used recursive method

---
+++ chapter 7/exercise /q1.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int func1(int a,int b);
+int func2(int a,int b);
+  
+int main()
+{
+
+	printf("%d %d\n",func1(3,8),func2(3,8));
+
+	return 0;
+}
+
+int func1(int a,int b)
+{
+	if (a>b) return 0;
+	return b+func1(a,b-1);
+}
+
+int func2(int a,int b)
+{
+	if (a>b) return 0;
+	return a+func2(a+1,b);
+}
