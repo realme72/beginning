c574d267ce2aa198547408300ca6b57fe623f968
realme72 <gauravbharti586@gmail.com>
2018-07-10 22:30:34 +0530

    getting sum and difference using recursion

---
+++ chapter 7/exercise /q3.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int func(int a);
+int func1(int a);
+
+int main()
+{
+	printf("%d\n",func(6));
+	printf("%d\n",func1(6));
+	return 0;
+}
+
+int func(int a)
+{
+	if(a==10)
+		return a;
+	return a+func(a+1);
+}
+
+int func1(int a)
+{
+	if(a==10)
+		return a;
+	return a+func(a-1);
+}
