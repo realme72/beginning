daf4ee74172213b96783c55a14de2c99b6ab740f
realme72 <gauravbharti586@gmail.com>
2018-07-29 18:52:29 +0530

    address of variable using address operator

---
+++ chapter 9/example/e1.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int age=30;
+	float salary=1500.50;
+
+	printf("Address of age=%p\n",&age);
+	printf("Address of salary=%p\n",&salary);
+
+	return 0;
+}
