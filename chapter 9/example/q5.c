3b94371ffc6a1c7c17f2690974b7351241a46809
realme72 <gauravbharti586@gmail.com>
2018-07-31 23:08:52 +0530

    incrementation/decrementation in a pointer variable of base type

---
+++ chapter 9/example/q5.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	int a=5;
+	int *p;
+	p=&a;
+
+	printf("value of p = Address of a = %p\n",p);
+	printf("value of p = %p\n",++p);
+	printf("value of p = %p\n",p++);
+	printf("value of p = %p\n",p);
+	printf("value of p = %p\n",--p);
+	printf("value of p = %p\n",p--);
+	printf("value of p = %p\n",p);
+
+	return 0;
+
+}
